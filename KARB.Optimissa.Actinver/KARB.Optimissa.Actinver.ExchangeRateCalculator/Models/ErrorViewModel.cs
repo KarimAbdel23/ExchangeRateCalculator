3f028478354dc29a0634e8d05feda34502bc1c89
Ryzen-5900X <kar_barrera@hotmail.com>
2025-09-30 14:58:23 -0600

    Commit inicial del proyecto Exchange Rate Calculator

---
+++ KARB.Optimissa.Actinver/KARB.Optimissa.Actinver.ExchangeRateCalculator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KARB.Optimissa.Actinver.ExchangeRateCalculator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
